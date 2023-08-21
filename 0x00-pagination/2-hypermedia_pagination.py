5a9311637a0efeff199f0eba89f84f74f7709911
Bolexzy <boluoyewumi79@gmail.com>
2023-08-21 01:41:22 +0100

    Fix: doc error

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -49,7 +49,13 @@ class Server:
         return []
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
-        '''
+        ''' Retuns a dictionary of:
+        page_size: the length of the returned dataset page
+        page: the current page number
+        data: the dataset page (equivalent to return from previous task)
+        next_page: number of the next page, None if no next page
+        prev_page: number of the previous page, None if no previous page
+        total_pages: the total number of pages in the dataset as an integer
         '''
         data = self.get_page(page, page_size)
         start, end = index_range(page, page_size)
